7a07ffb51928ec6e08ecce562ac3382a449635fe
pwndex <pwndex42@gmail.com>
2021-01-29 16:01:43 +0500

    feat: add navigation bar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <template>
   <div>
+    <mcv-topbar />
     Vue Medium Clone
-
     <router-view />
   </div>
 </template>
+
+<script>
+import McvTopbar from '@/components/Topbar'
+
+export default {
+  name: 'McvApp',
+  components: {
+    McvTopbar
+  }
+}
+</script>
