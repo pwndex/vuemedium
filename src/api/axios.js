39ba76b45f03054716f1575b88680454ba14e316
pwndex <pwndex42@gmail.com>
2021-01-30 01:32:33 +0500

    refactor: update axios with old api url & fix token prefix

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,11 +4,11 @@ import {getItem} from '@/helpers/persistanceStorage'
 // https://conduit.productionready.io/api/
 // http://37.46.130.143/api'
 
-axios.defaults.baseURL = 'https://conduit.productionready.io/api/'
+axios.defaults.baseURL = 'http://37.46.130.143/api'
 
 axios.interceptors.request.use(config => {
   const token = getItem('accessToken')
-  config.headers.Authorization = token ? `Token ${token}` : ''
+  config.headers.Authorization = token ? `Bearer ${token}` : ''
   return config
 })
 
