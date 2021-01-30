1f9b3c5ef14973cb42a757617777874e03aa4805
pwndex <pwndex42@gmail.com>
2021-01-30 18:09:25 +0500

    feat: style Feed component & list all articles

--- src/components/Feed.vue
+++ src/components/Feed.vue
@@ -1,7 +1,45 @@
 <template>
   <div>
-    FEED
-    {{ feedData }}
+    <div v-if="isLoading">Loading...</div>
+    <div v-if="error">Errors...</div>
+    <div v-if="feed">
+      <div
+        class="article-preview"
+        v-for="(article, index) in feed.articles"
+        :key="index"
+      >
+        <div class="article-meta">
+          <router-link
+            :to="{name: 'userProfile', params: {slug: article.author.username}}"
+          >
+            <img :src="article.author.image" alt="" />
+          </router-link>
+          <div class="info">
+            <router-link
+              :to="{
+                name: 'userProfile',
+                params: {slug: article.author.username}
+              }"
+              class="author"
+            >
+              {{ article.author.username }}
+            </router-link>
+            <span class="date">{{ article.createdAt }}</span>
+          </div>
+          <div class="pull-xs-right">Add To Favorites</div>
+        </div>
+        <router-link
+          :to="{name: 'article', params: {slug: article.slug}}"
+          class="preview-link"
+        >
+          <h1>{{ article.slug }}</h1>
+          <p>{{ article.description }}</p>
+          <span>Read more...</span>
+          TAG LIST
+        </router-link>
+      </div>
+      PAGINATION
+    </div>
   </div>
 </template>
 
@@ -19,7 +57,9 @@ export default {
   },
   computed: {
     ...mapState({
-      feedData: state => state.feed.data
+      isLoading: state => state.feed.isLoading,
+      feed: state => state.feed.data,
+      error: state => state.feed.error
     })
   },
   mounted() {
