4439d71b1d39f579588ac1d74b44a88c7d72b2ab
pwndex <pwndex42@gmail.com>
2021-01-30 16:22:58 +0500

    feat: add feed component & feed vuex, api modules

---
+++ src/components/Feed.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    FEED
+    {{ feedData }}
+  </div>
+</template>
+
+<script>
+import {mapState} from 'vuex'
+import {actionTypes} from '@/store/modules/feed'
+
+export default {
+  name: 'McvFeed',
+  props: {
+    apiUrl: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    ...mapState({
+      feedData: state => state.feed.data
+    })
+  },
+  mounted() {
+    this.$store.dispatch(actionTypes.getFeed, {apiUrl: this.apiUrl})
+  }
+}
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import auth from '@/store/modules/auth'
+import feed from '@/store/modules/feed'
 
 Vue.use(Vuex)
 
@@ -8,5 +9,5 @@ export default new Vuex.Store({
   state: {},
   mutations: {},
   actions: {},
-  modules: {auth}
+  modules: {auth, feed}
 })
