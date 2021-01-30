2513257c16d61b47392a7224f4fe3758590f6581
pwndex <pwndex42@gmail.com>
2021-01-30 20:46:38 +0500

    feat: add pagination component

---
+++ src/components/Pagination.vue
@@ -0,0 +1,48 @@
+<template>
+  <ul class="pagination">
+    <li
+      v-for="page in pages"
+      :key="page"
+      class="page-item"
+      :class="{active: currentPage === page}"
+    >
+      <router-link class="page-link" :to="{path: url, query: {page: page}}">
+        {{ page }}
+      </router-link>
+    </li>
+  </ul>
+</template>
+
+<script>
+import {range} from '@/helpers/utils'
+
+export default {
+  name: 'McvPagination',
+  props: {
+    url: {
+      type: String,
+      required: true
+    },
+    total: {
+      type: Number,
+      required: true
+    },
+    limit: {
+      type: Number,
+      required: true
+    },
+    currentPage: {
+      type: Number,
+      required: true
+    }
+  },
+  computed: {
+    pages() {
+      const pagesCount = Math.ceil(this.total / this.limit)
+      return range(1, pagesCount)
+    }
+  }
+}
+</script>
+
+<style scoped></style>
