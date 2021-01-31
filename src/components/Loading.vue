c31ec00635641b3ef3a6e08254a618e42c179e8b
pwndex <pwndex42@gmail.com>
2021-01-31 16:16:09 +0500

    feat: add Banner, Loading & ErrorMessage components

---
+++ src/components/Loading.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <ring color="#888888" class="spinner" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'McvLoading'
+}
+</script>
+
+<style scoped>
+.spinner {
+  margin: 100px auto;
+}
+</style>
