410ea6a7d74a04597b5b254a63162bcab9e08b87
pwndex <pwndex42@gmail.com>
2021-02-02 19:55:48 +0500

    feat: add logic to create article in createArticle view component

--- src/views/CreateArticle.vue
+++ src/views/CreateArticle.vue
@@ -1,20 +1,22 @@
 <template>
-  <div>
-    <div>Create Article</div>
-    <mcv-article-form
-      :initial-values="initialValues"
-      :errors="validationErrors"
-      :is-submitting="isSubmitting"
-      @articleSubmit="onSubmit"
-    />
-  </div>
+  <mcv-article-form
+    :initial-values="initialValues"
+    :errors="validationErrors"
+    :is-submitting="isSubmitting"
+    @articleSubmit="onSubmit"
+  />
 </template>
 
 <script>
+import {mapState} from 'vuex'
 import McvArticleForm from '@/components/ArticleForm'
+import {actionTypes} from '@/store/modules/createArticle'
 
 export default {
   name: 'McvCreateArticle',
+  components: {
+    McvArticleForm
+  },
   data() {
     return {
       initialValues: {
@@ -22,17 +24,22 @@ export default {
         description: '',
         body: '',
         tagList: []
-      },
-      validationErrors: null,
-      isSubmitting: false
+      }
     }
   },
-  components: {
-    McvArticleForm
+  computed: {
+    ...mapState({
+      isSubmitting: state => state.createArticle.isSubmitting,
+      validationErrors: state => state.createArticle.validationErrors
+    })
   },
   methods: {
-    onSubmit(data) {
-      console.log('onSubmit in createArticle', data)
+    onSubmit(articleInput) {
+      this.$store
+        .dispatch(actionTypes.createArticle, {articleInput})
+        .then(article => {
+          this.$router.push({name: 'article', params: {slug: article.slug}})
+        })
     }
   }
 }
