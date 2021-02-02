5548b3d10928299286f10e46597c5188b634e748
pwndex <pwndex42@gmail.com>
2021-02-02 17:17:38 +0500

    feat: add CreateArticle view component & use ArticleForm component

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import GlobalFeed from '@/views/GlobalFeed.vue'
 import YourFeed from '@/views/YourFeed.vue'
 import TagFeed from '@/views/TagFeed.vue'
 import Article from '@/views/Article'
+import CreateArticle from '@/views/CreateArticle'
 
 Vue.use(VueRouter)
 
@@ -38,7 +39,7 @@ const routes = [
   {
     path: '/articles/new',
     name: 'createArticle',
-    component: GlobalFeed
+    component: CreateArticle
   },
   {
     path: '/articles/:slug',
---
+++ src/views/CreateArticle.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div>Create Article</div>
+    <mcv-article-form
+      :initial-values="initialValues"
+      :errors="validationErrors"
+      :is-submitting="isSubmitting"
+      @articleSubmit="onSubmit"
+    />
+  </div>
+</template>
+
+<script>
+import McvArticleForm from '@/components/ArticleForm'
+
+export default {
+  name: 'McvCreateArticle',
+  data() {
+    return {
+      initialValues: {
+        title: '',
+        description: '',
+        body: '',
+        tagList: []
+      },
+      validationErrors: null,
+      isSubmitting: false
+    }
+  },
+  components: {
+    McvArticleForm
+  },
+  methods: {
+    onSubmit(data) {
+      console.log('onSubmit in createArticle', data)
+    }
+  }
+}
+</script>
+
+<style scoped></style>
