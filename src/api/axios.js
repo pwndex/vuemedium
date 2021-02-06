a8ea2119e24f183ae3cac96a5b345ad82978e71a
pwndex <pwndex42@gmail.com>
2021-02-06 16:26:26 +0500

    refactor: update api & router & vue.config.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,11 +1,12 @@
 import axios from 'axios'
 import {getItem} from '@/helpers/persistanceStorage'
-
-axios.defaults.baseURL = 'http://37.46.130.143/api'
+// https://conduit.productionready.io/api
+// http://37.46.130.143/api
+axios.defaults.baseURL = 'https://conduit.productionready.io/api'
 
 axios.interceptors.request.use(config => {
   const token = getItem('accessToken')
-  config.headers.Authorization = token ? `Bearer ${token}` : ''
+  config.headers.Authorization = token ? `Token ${token}` : ''
   return config
 })
 
